c20cf99895cd6e3ab72fa95911133eeb92623dfa
isaaconek <onekdev5@gmail.com>
2025-07-16 20:57:29 +0100

    Premier texte de la page d'accueil

--- screens/accueill/accueill.tsx
+++ screens/accueill/accueill.tsx
@@ -7,7 +7,8 @@ const Accueill = () => {
             <ScrollView className='w-full pt-20' >
                 <View className='items-center gap-5'>
                     <Image source={require('../../assets/avatar-cover.jpg')}  className='w-[90%] h-44 rounded-xl ' />
-                    <Text className='text-red-600 text-[1.5rem] font-semibold text-left '>Votre plaisir commence ici</Text>                    
+                        <Text className='text-red-600 text-[1.5rem] font-semibold text-left '>Votre plaisir commence ici</Text>
+                        <Text className='text-white px-10 text-center'>Découvrez les meilleurs films et series actuels et explorer le sptième art avec une experience inedit...</Text>                    
                 </View>
             </ScrollView>
         </View>
