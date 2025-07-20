30baa906a7750afc4c6eacd0a3e0a6398a64c00e
isaaconek <onekdev5@gmail.com>
2025-07-20 19:09:56 +0100

    Installation de dépendances et configuration pour le support du fichier .env

--- screens/accueill/components/popularMovie.tsx
+++ screens/accueill/components/popularMovie.tsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { View } from 'react-native';
+import { useHomeData } from 'store/homeData';
 
 const PopularMovie = () => {
     const [ loading, setLoading ] = useState(false)
+    const popularMovies = useHomeData((state) => state.popularMovies)
+    const setPopularMovies = useHomeData((state) => state.setPopularMovies)
+
+    useEffect(() => {
+        if(!popularMovies || popularMovies.length === 0 ) {
+            axios.get("")
+            .then(() => {
+                
+            })
+        }
+    }, [])
     return (
         <View>
             
