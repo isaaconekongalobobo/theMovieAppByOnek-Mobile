058e1603396dbe4a1907438896d0ede51048ef4f
isaaconek <onekdev5@gmail.com>
2025-08-01 12:05:41 +0100

    Rajout dans le state useHomeData des propriétés pour les series

--- store/homeData.ts
+++ store/homeData.ts
@@ -1,12 +1,14 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { Movie } from "types/allType";
+import Series from "screens/series/series";
+import { Movie, Serie } from "types/allType";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 
 interface HomeDataType {
     popularMovies: Movie[] | [],
     setPopularMovies: (movies: Movie[]) => void,
-    popularSeries: Serie
+    popularSeries: Serie[] | [],
+    setPopularSeries: (series: Serie[]) => void,
 }
 
 export const useHomeData = create<HomeDataType>()(
@@ -14,7 +16,8 @@ export const useHomeData = create<HomeDataType>()(
         (set) => ({
             popularMovies: [],
             setPopularMovies: (movies: Movie[]) => set({ popularMovies: movies }),
-            popularSeries: 
+            popularSeries: [],
+            setPopularSeries: (series: Serie[]) => set({ popularSeries: series })
         }),
         {
         name: 'home-page-data',
