2b485e2b3790453442e78ee7f3fd428e21717279
isaaconek <onekdev5@gmail.com>
2025-08-01 10:50:39 +0100

    Remplacement du composant View par TouchableOpacity dans la carte des fimls populaire pour la rendre cliquable

--- screens/accueill/components/movieCard.tsx
+++ screens/accueill/components/movieCard.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import { View, Text, Image } from 'react-native';
+import { Text, Image, TouchableOpacity } from 'react-native';
 import { Movie } from 'types/allType';
 import { TMBD_IMG_BASE_URL } from '@env'
 
@@ -10,12 +10,12 @@ interface MovieCardProps {
 
 const MovieCard = ({ movie }: MovieCardProps) => {
   return (
-    <View className="w-[140px] mr-3 items-center">
+    <TouchableOpacity className="w-[140px] mr-3 items-center">
       <Image source={{ uri: TMBD_IMG_BASE_URL + movie.poster_path }} className="w-[140px] h-[210px] rounded-lg" resizeMode="cover"/>
       <Text numberOfLines={1} className="mt-2 text-sm font-semibold text-white text-center">
         {movie.title}
       </Text>
-    </View>
+    </TouchableOpacity>
   );
 };
 
