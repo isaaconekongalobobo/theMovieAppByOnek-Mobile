3cac85419d2a78c8bf58541af5410fb737aff1b6
isaaconek <onekdev5@gmail.com>
2025-08-03 09:23:23 +0100

    Installation de la lib react-native-youtube-iframe pour afficher le trailler avec plus de controlle

---
+++ screens/films/components/movieTrailer.tsx
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import { Text, View, ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native-webview';
+import axios from 'axios';
+
+interface MovieTrailerProps {
+  movieId: number;
+}
+
+const TMDB_API_KEY = process.env.EXPO_PUBLIC_TMDB_API_KEY;
+
+const MovieTrailer = ({ movieId }: MovieTrailerProps) => {
+  const [trailerUrl, setTrailerUrl] = useState<string | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchTrailer = async () => {
+      try {
+        const res = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/videos?api_key=${TMDB_API_KEY}&language=fr-FR`
+        );
+
+        const trailers = res.data.results.filter(
+          (video: any) => video.type === 'Trailer' && video.site === 'YouTube'
+        );
+
+        if (trailers.length > 0) {
+          const embedUrl = `https://www.youtube.com/embed/${trailers[0].key}`;
+          setTrailerUrl(embedUrl);
+        }
+      } catch (error) {
+        console.error('Erreur trailer :', error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchTrailer();
+  }, [movieId]);
+
+  if (loading) {
+    return <ActivityIndicator size="large" color="#E53935" />;
+  }
+
+  if (!trailerUrl) {
+    return (
+      <Text className="text-center text-base text-neutral-600 mt-4">
+        Aucune bande-annonce trouvée.
+      </Text>
+    );
+  }
+
+  return (
+    <View className="w-full aspect-video rounded-xl overflow-hidden my-4">
+      <WebView className="flex-1" javaScriptEnabled domStorageEnabled source={{ uri: trailerUrl }}/>
+    </View>
+  );
+};
+
+export default MovieTrailer;
